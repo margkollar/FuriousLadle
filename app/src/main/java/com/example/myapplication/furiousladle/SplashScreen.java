0e83ae29a979bad6904d194199d1a8dff6ba9d80
Margarita <margaritakollarou13@gmail.com>
2021-02-18 15:33:45 +0200

    splash screen

--- app/src/main/java/com/example/myapplication/furiousladle/SplashScreen.java
+++ app/src/main/java/com/example/myapplication/furiousladle/SplashScreen.java
@@ -5,20 +5,16 @@ import android.annotation.SuppressLint;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
+
+
 public class SplashScreen extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
+
+
+
 //    private static final boolean AUTO_HIDE = true;
 //
 //    /**
