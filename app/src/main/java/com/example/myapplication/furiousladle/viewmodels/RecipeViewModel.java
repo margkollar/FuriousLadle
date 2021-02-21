ffb9f23c1ebd980f69a5d41bd6a20507ef068622
cemmanouil <cemmanouil@athtech.gr>
2021-02-21 11:23:16 +0200

    RecipeViewModel in progress

--- app/src/main/java/com/example/myapplication/furiousladle/viewmodels/RecipeViewModel.java
+++ app/src/main/java/com/example/myapplication/furiousladle/viewmodels/RecipeViewModel.java
@@ -5,24 +5,30 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class RecipeViewModel extends AndroidViewModel {
     private final String appID = "eb3439ca";
     private final String appKey = "cc857c2c04f1702d19d6bc1fa6099c04";
     private final String baseAPIPath = "https://api.edamam.com/search";
+    private RequestQueue queue;
 
 //    @NonNull
 //    private Request queue;
 
     public RecipeViewModel(@NonNull Application application) {
         super(application);
+        queue = Volley.newRequestQueue(application);
     }
+
     public String prepareRequest(@NonNull String parameter){
         return String.format("%s?app_id=%s&app_key=%s&q=%s",
                 baseAPIPath, appID, appKey, parameter);
 
     }
 
-    void retrieveProgram(RecipeListener callback) {
+    void retrieveRecipe(RecipeListener callback) {
 
     }
 }
