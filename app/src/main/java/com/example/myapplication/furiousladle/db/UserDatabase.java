5ffbfa789fc50f7f67bf27d21571a598262cdd24
cemmanouil <cemmanouil@athtech.gr>
2021-02-26 12:27:23 +0200

    UserDatabase created

---
+++ app/src/main/java/com/example/myapplication/furiousladle/db/UserDatabase.java
@@ -0,0 +1,11 @@
+package com.example.myapplication.furiousladle.db;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities= {UserEntity.class}, version = 1)
+public abstract class UserDatabase extends RoomDatabase {
+    public abstract UserDao userDao();
+
+}
